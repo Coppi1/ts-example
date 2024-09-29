1e990a52097b9f9f68e5bc8a1aeca54b47f6056d
RaphaelAml <raphaelaml1@outlook.com>
2024-09-29 09:30:46 -0300

    Acertos para branch raphael

--- dist/shared/connection.js
+++ dist/shared/connection.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize"; // Use sequelize ao invés de @sequelize/core
-const sequelize = new Sequelize("bd_ts_example", "root", "1234", {
+const sequelize = new Sequelize("bd_ts_example", "root", "05689471", {
     host: "localhost",
     dialect: "mysql", // Aqui você pode usar 'mysql'
 });
